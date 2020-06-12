2b8b8ac7c0a2b0fa89a81a9d732d2f0a28e26dcd
Keerthi Mani <carkeane.getz@gmail.com>
2020-06-12 13:08:14 -0400

    initialised all the buttons to a variable

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const deleteButton = document.querySelector("[data-delete]");
+const equalButton = document.querySelector("[data-equal]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
